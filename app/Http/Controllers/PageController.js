d6335ac951d750588b591dc60db91db3807e77ff
Navdeep Singh <navdeep.er@gmail.com>
2017-01-17 12:11:48 +0530

    update

--- app/Http/Controllers/PageController.js
+++ app/Http/Controllers/PageController.js
@@ -17,53 +17,54 @@ class PageController {
     yield response.sendView( 'welcome', {twitterFeed : user} )
   }
 
-  * analyze (request, response) { 
-              
+  * analyze (request, response) {
+
     const twitterAccessToken = yield request.session.get('twitterAccessToken')
     const twitterUser = yield UserTwitter.findBy( 'access_token', twitterAccessToken )
-        
+
     const facebookAccessToken = yield request.session.get('facebookAccessToken')
     const facebookUser = yield UserFacebook.findBy( 'access_token', facebookAccessToken )
 
     const twitterFeeds =  yield twitterUser.feeds().fetch()
     const facebookFeeds = yield facebookUser.feeds().fetch()
 
-    const sentiment = promisify( alchemyapi.sentiment.bind( alchemyapi ))   
+    const sentiment = promisify( alchemyapi.sentiment.bind( alchemyapi ))
 
     const socialFeeds = {"twitter": {"table": "twitter_feed","data": twitterFeeds}, "facebook" : {"table": "facebook_feed", "data" : facebookFeeds}}
 
 
-    for (let key in socialFeeds) {        
+    for (let key in socialFeeds) {
          if (socialFeeds.hasOwnProperty(key)) {
             console.log(socialFeeds[key].table)
             let index = 0
             let feedJson = socialFeeds[key].data.toJSON()
             while( index < feedJson.length ) {
- 
-              let feed = feedJson[index].feed    
+
+              let feed = feedJson[index].feed
 
               try {
-                  yield sentiment("text", feed, {})      
-              } 
+                  yield sentiment("text", feed, {})
+              }
               catch(res) {
+                  console.log(res)
                   yield Database.table(socialFeeds[key].table)
                   .where('id', feedJson[index].id)
-                  .update({'analysis' :  JSON.stringify(res.docSentiment), updated_at : moment().format('YYYY-MM-DD hh:mm:ss')})            
+                  .update({'analysis' :  JSON.stringify(res.docSentiment), updated_at : moment().format('YYYY-MM-DD hh:mm:ss')})
               }
               index++
-            }                
-         }     
+            }
+         }
     }
     yield request.session.put('analyzingComplete', 'ok')
-    return response.send('ok')    
+    return response.send('ok')
 
   }
 
   * results (request, response) {
-    
+
     const twitterAccessToken = yield request.session.get('twitterAccessToken')
     const twitterUser = yield UserTwitter.findBy( 'access_token', twitterAccessToken )
-  
+
     const facebookAccessToken = yield request.session.get('facebookAccessToken')
     const facebookUser = yield UserFacebook.findBy( 'access_token', facebookAccessToken )
 
@@ -115,7 +116,7 @@ class PageController {
     }
 
     if (twitterPulled == null) {
-      socialStatus.pulledTwitter = false     
+      socialStatus.pulledTwitter = false
     } else {
        socialStatus.pulledTwitter = true
     }
